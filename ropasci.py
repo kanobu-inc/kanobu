9c550dea7a417bc21ef67b6e9734fa4aaba8d732
jDan735 <daniel734@bk.ru>
2020-05-24 20:54:37 +0300

    Optimizations, added debug mode

--- ropasci.py
+++ ropasci.py
@@ -1,32 +1,43 @@
 import random
 import time
 import json
-from colorama import init, Fore, Back, Style
-from termcolor import colored
+import os
+from colorama import init, Fore, Back
 
 init()
+
+# Want colorama
+# Want os
+# Work if dev == True
+def log(text, space=False):
+    if dev:
+        if space:
+            print(Fore.YELLOW + " [DEV] " + Fore.WHITE + text)
+        else:
+            print(Fore.YELLOW + "[DEV] " + Fore.WHITE + text)
+
+if len(os.sys.argv) > 1:
+    dev = os.sys.argv[1] == "dev"
 
 lang = input('Your language? (de, en, ru, ua, em) ')
 
 while lang != "de" and lang != "en" and lang != "ru" and lang != "ua" and lang != "em":
-    lang = input('Your language? (de, en, ru, ua) ')
+    lang = input('Your language? (de, en, ru, ua, em) ')
 
 with open("./locale/" + lang + ".json", encoding="utf-8") as locale_file:
     locale = json.load(locale_file)
+    log(locale["name"])
 
-spaces = ""
-for i in range(len(locale["name"]) + 2):
-    spaces += " "
+spaces = " " * (len(locale["game"]) + 2)
 
 print(Back.BLUE + spaces + Back.BLACK)
-print(Back.BLUE + " " + locale["name"] + " " + Back.BLACK)
+print(Back.BLUE + " " + locale["game"] + " " + Back.BLACK)
 print(Back.BLUE + spaces + Back.BLACK)
 
 while True:
 
-    print('1. ' + locale["objects"][0])
-    print('2. ' + locale["objects"][1])
-    print('3. ' + locale["objects"][2])
+    for key in range(3):
+        print(str(key + 1) + ". " + locale["objects"][key])
 
     player = int(input(locale["message"]["requests"]["choice"])) - 1
     print()
@@ -37,27 +48,36 @@ while True:
     time.sleep(1)
     bot = random.randint(0, 2)
 
+    # ========== SHITCODES ==========
+    def win():
+        print(Fore.GREEN + " " + locale["results"][0] + "!" + Fore.WHITE + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
+    def lose():
+        print(Fore.RED + " " + locale["results"][1] + "!" + Fore.WHITE + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
+    def draw():
+        print(Fore.YELLOW + " " + locale["results"][2] + "!" + Fore.WHITE + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
+    # ===============================
+
     if bot == 0:
         if player == 2:
-            print(Fore.RED + " " + locale["results"][1] + "!" + Fore.WHITE + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
+            lose()
         if player == 1:
-            print(Fore.GREEN + " " + locale["results"][0] + "!" + Fore.WHITE + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
+            win()
         if player == 0:
-            print(Fore.YELLOW + " " + locale["results"][2] + "!" + Fore.WHITE + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
+            draw()
     if bot == 1:
         if player == 2:
-            print(Fore.GREEN + " " + locale["results"][0] + "!" + Fore.WHITE + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
+            win()
         if player == 1:
-            print(Fore.YELLOW + " " + locale["results"][2] + "!" + Fore.WHITE + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
+            draw()
         if player == 0:
-            print(Fore.RED + " " + locale["results"][1] + "!" + Fore.WHITE + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
+            lose()
     if bot == 2:
         if player == 2:
-            print(Fore.YELLOW + " " + locale["results"][2] + "!" + Fore.WHITE + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
+            draw()
         if player == 1:
-            print(Fore.GREEN + " " + locale["results"][0] + "!" + Fore.WHITE + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
+            win()
         if player == 0:
-            print(Fore.RED + " " +locale["results"][1] + "!" + Fore.WHITE + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
+            lose()
 
     print()
 
