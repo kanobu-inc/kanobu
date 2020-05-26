9fd961f16a2fc95fe48bee44a3928385d8bf8563
jDan735 <daniel734@bk.ru>
2020-05-26 10:00:36 +0300

    Delete comments

--- ropasci.py
+++ ropasci.py
@@ -15,12 +15,14 @@ from color import *
 #REWRITE
 def log(text, leftSpace=False, rightSpace=True):
     if dev:
-        leftSpace = ""
-        rightSpace = ""
         if leftSpace:
             leftSpace = " "
+        else:
+            leftSpace = ""
         if rightSpace:
             rightSpace = " "
+        else:
+            rightSpace = ""
         print(leftSpace + yellow("[DEV]") + rightSpace + text)
 
 if len(os.sys.argv) > 1:
@@ -57,16 +59,6 @@ while True:
     time.sleep(1)
     bot = random.randint(0, 2)
 
-    # ========== SHITCODES ==========
-    def win():
-        print(green(" " + locale["results"][0] + "!") + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
-    def lose():
-        print(red(" " + locale["results"][1] + "!") + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
-    def draw():
-        print(yellow(" " + locale["results"][2] + "!") + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
-    # ===============================
-
-    #FIXME: Rename var
     with open("./config.cson", "r", encoding="utf-8") as config_file:
         modes = cson.load(config_file)
     massive = modes["standart"]
@@ -81,17 +73,6 @@ while True:
             if i == 2:
                 print(red(" " + locale["results"][key] + "!") + " " + locale["bot"]["have"] + " " + locale["objects"][bot])
         i += 1
-
-
-    # if bot == 0:
-    #     if player == 0:
-    #         draw()
-    #     if player == 1:
-    #         win()
-    #     if player == 2:
-    #         lose()
-
-
 
     print()
 
