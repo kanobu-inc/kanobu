0b5583ca051efd0409dac697c3f28da522a0160b
jDan735 <daniel734@bk.ru>
2020-05-26 11:52:41 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from cx_Freeze import setup, Executable
 
 setup(
     name="kanobu",
+    extras_require = {
+       "build": ["cson", "colorama"]
+    },
     executables=[Executable("ropasci.py")]
 )
