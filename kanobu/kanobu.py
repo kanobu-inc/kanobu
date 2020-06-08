a94fdbb37d0dd46056237b515076057f1562eb93
jDan735 <daniel734@bk.ru>
2020-06-08 19:26:23 +0300

    Move color function from main to color.py

--- kanobu/kanobu.py
+++ kanobu/kanobu.py
@@ -5,28 +5,7 @@ def main():
     import time
     import cson
     import argparse
-    from colorama import init
-
-    init()
-
-    def red(text):
-        return "\033[31m" + text + "\033[0m"
-
-    def green(text):
-        return "\033[32m" + text + "\033[0m"
-
-    def yellow(text):
-        return "\033[33m" + text + "\033[0m"
-
-    def blue(text):
-        return "\033[44m" + text + "\033[0m"
-
-    def logo(name):
-        spaces = " " * (len(name) + 2)
-
-        print(blue(spaces))
-        print(blue(" " + name + " "))
-        print(blue(spaces))
+    from kanobu.color import red, green, yellow, blue, logo
 
     #REWRITE
     def log(text, left=False, right=True):
