4e8c289017625c72f7ec2583e2bc4e036b3629c8
jDan735 <daniel734@bk.ru>
2020-06-08 20:37:50 +0300

    Update test

--- tests/lang.py
+++ tests/lang.py
@@ -6,12 +6,12 @@ os.chdir(os.path.join(os.path.abspath(os.path.curdir)))
 
 python = "python3" if os.name == "posix" or os.name == "macos" else "python"
 
-os.system(python + " -m kanobu --lang en")
-os.system(python + " -m kanobu --lang em")
-os.system(python + " -m kanobu --lang de")
-os.system(python + " -m kanobu --lang fr")
-os.system(python + " -m kanobu --lang it")
-os.system(python + " -m kanobu --lang ru")
-os.system(python + " -m kanobu --lang ua")
+os.system(python + " -m kanobu --lang en --choice 1")
+os.system(python + " -m kanobu --lang em --choice 1")
+os.system(python + " -m kanobu --lang de --choice 1")
+os.system(python + " -m kanobu --lang fr --choice 1")
+os.system(python + " -m kanobu --lang it --choice 1")
+os.system(python + " -m kanobu --lang ru --choice 1")
+os.system(python + " -m kanobu --lang ua --choice 1")
 
 os.chdir(owd)
